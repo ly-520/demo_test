d75b105a99419b76c0cf61014e605fb4caef0072
liujitong <liujitong@yonrun.com>
2019-10-09 20:00:59 +0800

    employee表

---
+++ src/main/java/com/example/demo_test/sql/employee.sql
@@ -0,0 +1,8 @@
+INSERT INTO `employee`(`id`, `name`, `phone`, `email`, `age`, `company`) VALUES (1, '张三', '15036890102', '15036890102@163.com', 24, '天天户外');
+INSERT INTO `employee`(`id`, `name`, `phone`, `email`, `age`, `company`) VALUES (2, '李四', '15036890103', '15036890103@163.com', 25, 'Tgroup工作室');
+INSERT INTO `employee`(`id`, `name`, `phone`, `email`, `age`, `company`) VALUES (3, '王二', '15036890104', '15036890104@163.com', 58, '天天户外俱乐部');
+INSERT INTO `employee`(`id`, `name`, `phone`, `email`, `age`, `company`) VALUES (4, '麻子', '15036890108', '15036890108@163.com', 18, '天天户外旗舰店');
+INSERT INTO `employee`(`id`, `name`, `phone`, `email`, `age`, `company`) VALUES (5, '翠花', '15036890125', '15036890125@163.com', 16, '野渡无人');
+INSERT INTO `employee`(`id`, `name`, `phone`, `email`, `age`, `company`) VALUES (6, '1', '1', '1', 1, NULL);
+INSERT INTO `employee`(`id`, `name`, `phone`, `email`, `age`, `company`) VALUES (7, '1', '1', '1', 1, NULL);
+INSERT INTO `employee`(`id`, `name`, `phone`, `email`, `age`, `company`) VALUES (8, '测试', '2', '2', 2, 'Error creating bean with name \'requestMappingHandlerMapping\' defined in class path resource');
